79bd23282e3ce55af42c3b6bb054ac5cd1e49a07
Vivek Prajapati <vivekprajapati2048@gmail.com>
2021-08-24 17:58:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png-dice6.png
+var randomImageSource = "images/" + randomDiceImage; // images/dice1.png-images/dice6.png
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// player wins
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩Player 2 Wins!";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
